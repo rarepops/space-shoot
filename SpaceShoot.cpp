32dd7fd3a350a618b18c65dc3346c98ca812f73d
Rares Popa <Rares_Popa_2008@Yahoo.com>
2017-11-25 16:50:34 +0100

    feat: add main entry point and expand game skeleton

--- SpaceShoot.cpp
+++ SpaceShoot.cpp
@@ -1,114 +1,44 @@
-#include <sre/Profiler.hpp>
-#include "GameObject.hpp"
-#include "sre/RenderPass.hpp"
-#include "sre/Texture.hpp"
-#include "Box2D/Dynamics/Contacts/b2Contact.h"
 #include "SpaceShoot.hpp"
-
-using namespace std;
-using namespace sre;
-
-const glm::vec2 SpaceShoot::windowSize(800, 600);
-
-SpaceShoot* SpaceShoot::instance = nullptr;
+#include <ctime>
 
 SpaceShoot::SpaceShoot()
-	:debugDraw(physicsScale)
 {
-	instance = this;
-	r.setWindowSize(windowSize);
-	bool useVsync = true;
-	r.init(SDL_INIT_EVERYTHING, SDL_WINDOW_OPENGL);
+    renderer.setWindowTitle("SpaceShoot");
+    renderer.init(SDL_INIT_EVERYTHING, SDL_WINDOW_OPENGL);
 
-	backgroundColor = glm::vec4(0.6, 0.6, 1, 1);
+    srand(time(NULL));
 
-	spriteAtlas = SpriteAtlas::create("space.json", "space.png");
+    init();
+    camera.setWindowCoordinates();
 
+    renderer.keyEvent = [&](SDL_Event& e) {
+        onKey(e);
+    };
+    renderer.frameUpdate = [&](float deltaTime) {
+        update(deltaTime);
+    };
+    renderer.frameRender = [&]() {
+        render();
+    };
 
-	initLevel();
-
-	// setup callback functions
-	r.keyEvent = [&](SDL_Event& e) {
-		onKey(e);
-	};
-	r.frameUpdate = [&](float deltaTime) {
-		update(deltaTime);
-	};
-	r.frameRender = [&]() {
-		render();
-	};
-	// start game loop
-	r.startEventLoop();
+    renderer.startEventLoop();
 }
 
-void SpaceShoot::initLevel()
+void SpaceShoot::init()
 {
-	initPhysics();
 
 }
 
 void SpaceShoot::update(float time)
 {
-	updatePhysics();
-	if (time > 0.03) // if framerate approx 30 fps then run two physics steps
-	{
-		updatePhysics();
-	}
-	for (int i = 0; i < sceneObjects.size(); i++)
-	{
-		sceneObjects[i]->update(time);
-	}
+}
+
+void SpaceShoot::onKey(SDL_Event & event)
+{
+
 }
 
 void SpaceShoot::render()
 {
-	
-}
-
-void SpaceShoot::onKey(SDL_Event &event)
-{
-	
-}
-
-std::shared_ptr<GameObject> SpaceShoot::createGameObject()
-{
 
 }
-
-void SpaceShoot::updatePhysics()
-{
-
-}
-
-void SpaceShoot::initPhysics()
-{
-	
-}
-
-void SpaceShoot::BeginContact(b2Contact *contact)
-{
-	b2ContactListener::BeginContact(contact);
-	handleContact(contact, true);
-}
-
-void SpaceShoot::EndContact(b2Contact *contact)
-{
-	b2ContactListener::EndContact(contact);
-	handleContact(contact, false);
-}
-
-void SpaceShoot::deregisterPhysicsComponent(PhysicsComponent *r)
-{
-	
-}
-
-void SpaceShoot::registerPhysicsComponent(PhysicsComponent *r)
-{
-	
-}
-
-void SpaceShoot::handleContact(b2Contact *contact, bool begin)
-{
-	
-}
-
